de9a76ad2cd532d94e1ab837ca0acd9c5a2040ea
Lucas Pedrolo <132529241+LucasSamuelPedrolo@users.noreply.github.com>
2023-06-14 23:30:23 -0300

    add js

--- javascript/script.js
+++ javascript/script.js
@@ -1,2 +1,28 @@
-const banner = document.querySelector('#banner')
+const bannerImg = document.querySelectorAll('.descontosBanner');
 
+let tempo = 5000;
+let imagemAtual = 0;
+let maxImgs = bannerImg.length;
+
+function mudaImg(){
+    bannerImg[imagemAtual].classList.remove('selected');
+    imagemAtual++;
+
+    if(imagemAtual >= maxImgs){
+        imagemAtual = 0;
+    }
+    bannerImg[imagemAtual].classList.add('selected');
+
+}
+
+const dataFinal = new Date();
+console.log(dataFinal)
+
+function start(){
+
+    setInterval(()=>{
+        mudaImg()
+    },tempo)
+}
+
+document.addEventListener('load',start());
